a2fd684b8cfef628958c2664b3b518219e564738
Ujwal Komarla <ujwal_komarla@ncsu.edu>
2014-11-27 03:10:58 -0500

    Float not working

--- distVecAlgo.c
+++ distVecAlgo.c
@@ -7,11 +7,11 @@ int main(int argc,char **argv){
 		sprintf(str,"USAGE: %s initial-node file-name node1 node2",argv[0]);
 		DieWithError(str);
 	}
-	const FILE *pTopologyFile = fopen(argv[2],"r");
-	const int iInitNode = atoi(argv[1]);
-	const int iNode1 = atoi(argv[3]);
-	const int iNode2 = atoi(argv[4]);
-
+	FILE *pTopologyFile = fopen(argv[2],"r");
+	int iInitNode = atoi(argv[1]);
+	int iNode1 = atoi(argv[3]);
+	int iNode2 = atoi(argv[4]);
+	int i,j;
 	int iNoOfNodes;
 	int iNoOfLinks;
 	if(0 == fscanf(pTopologyFile, "%d", &iNoOfNodes)) DieWithError("File Read: Topology file, Number of nodes");
@@ -19,7 +19,32 @@ int main(int argc,char **argv){
 	#ifdef DEBUG
 		printf("Number of links : %d\r\n",iNoOfLinks);
 	#endif
-
+	int *links = malloc(sizeof(int) * iNoOfLinks * 2); //2 -> Node1 Node2 
+	float *linkCost = malloc(sizeof(float) * iNoOfLinks);
+	for(i=0; i<iNoOfLinks; i++ ){
+		fscanf(pTopologyFile, "%d %d %f", (links + i*3 + 0), (links + i*3 + 1), (linkCost + i));
+	#ifdef DEBUG
+		printf("Iteration %d : %d %d %f\r\n",i, *(links + i*3 + 0), *(links + i*3 + 1), *(linkCost + i));
+	#endif
+	}
+	float *overallMatrix = malloc(sizeof(float) * iNoOfNodes * iNoOfNodes);
+	memset(overallMatrix, 0 , sizeof(float) * iNoOfNodes * iNoOfNodes);// -1 not a neighbour
+	for(i=0; i<iNoOfLinks; i++){
+		*(overallMatrix + (*(links + i*3 + 0)-1)*iNoOfNodes + *(links + i*3 + 1)-1) = *(linkCost+i);//Neighbours
+		*(overallMatrix + (*(links + i*3 + 1)-1)*iNoOfNodes + *(links + i*3 + 0)-1) = *(linkCost+i);//Neighbours
+	}
+	for(i=0;i<iNoOfNodes;i++) *(overallMatrix + i*iNoOfNodes + i) = 0;
+	#ifdef DEBUG
+	printf("   ");
+	for(i=1;i<iNoOfNodes+1;i++) printf("%4d ", i);
+	printf("\r\n");
+	for(i=0;i<iNoOfNodes;i++){
+		printf("%4d", i+1);	
+		for(j=0;j<iNoOfNodes;j++)
+			printf("%4.1f ", *(overallMatrix + i*iNoOfNodes + j));
+		printf("\r\n");	
+	}
+	#endif
 
 
 return 0;
