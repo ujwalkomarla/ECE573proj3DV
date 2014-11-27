8be545e36d340b9d720b5c2c8f9118e0c5d96ab9
Ujwal Komarla <ujwal_komarla@ncsu.edu>
2014-11-27 03:13:13 -0500

    Reverted to integer Values for link cost

--- distVecAlgo.c
+++ distVecAlgo.c
@@ -19,29 +19,28 @@ int main(int argc,char **argv){
 	#ifdef DEBUG
 		printf("Number of links : %d\r\n",iNoOfLinks);
 	#endif
-	int *links = malloc(sizeof(int) * iNoOfLinks * 2); //2 -> Node1 Node2 
-	float *linkCost = malloc(sizeof(float) * iNoOfLinks);
+	int *links = malloc(sizeof(int) * iNoOfLinks * 3); //3 -> Node1 Node2 LinkCost
 	for(i=0; i<iNoOfLinks; i++ ){
-		fscanf(pTopologyFile, "%d %d %f", (links + i*3 + 0), (links + i*3 + 1), (linkCost + i));
+		fscanf(pTopologyFile, "%d %d %d", (links + i*3 + 0), (links + i*3 + 1), (links + i*3 + 2));
 	#ifdef DEBUG
-		printf("Iteration %d : %d %d %f\r\n",i, *(links + i*3 + 0), *(links + i*3 + 1), *(linkCost + i));
+		printf("Iteration %d : %d %d %d\r\n",i, *(links + i*3 + 0), *(links + i*3 + 1), *(links + i*3 + 2));
 	#endif
 	}
-	float *overallMatrix = malloc(sizeof(float) * iNoOfNodes * iNoOfNodes);
-	memset(overallMatrix, 0 , sizeof(float) * iNoOfNodes * iNoOfNodes);// -1 not a neighbour
+	int *overallMatrix = malloc(sizeof(int) * iNoOfNodes * iNoOfNodes);
+	memset(overallMatrix, -1, sizeof(int) * iNoOfNodes * iNoOfNodes);// -1 not a neighbour
 	for(i=0; i<iNoOfLinks; i++){
-		*(overallMatrix + (*(links + i*3 + 0)-1)*iNoOfNodes + *(links + i*3 + 1)-1) = *(linkCost+i);//Neighbours
-		*(overallMatrix + (*(links + i*3 + 1)-1)*iNoOfNodes + *(links + i*3 + 0)-1) = *(linkCost+i);//Neighbours
+		*(overallMatrix + (*(links + i*3 + 0)-1)*iNoOfNodes + *(links + i*3 + 1)-1) = *(links + i*3 + 2);//Neighbours
+		*(overallMatrix + (*(links + i*3 + 1)-1)*iNoOfNodes + *(links + i*3 + 0)-1) = *(links + i*3 + 2);//Neighbours
 	}
 	for(i=0;i<iNoOfNodes;i++) *(overallMatrix + i*iNoOfNodes + i) = 0;
 	#ifdef DEBUG
 	printf("   ");
-	for(i=1;i<iNoOfNodes+1;i++) printf("%4d ", i);
+	for(i=1;i<iNoOfNodes+1;i++) printf("%3d ", i);
 	printf("\r\n");
 	for(i=0;i<iNoOfNodes;i++){
-		printf("%4d", i+1);	
+		printf("%3d", i+1);	
 		for(j=0;j<iNoOfNodes;j++)
-			printf("%4.1f ", *(overallMatrix + i*iNoOfNodes + j));
+			printf("%3d ", *(overallMatrix + i*iNoOfNodes + j));
 		printf("\r\n");	
 	}
 	#endif
